aa8ac697368c09c3bbdcc1de1b0a9388b9faad34
EC2 Default User <ec2-user@ip-172-31-27-82.ap-northeast-1.compute.internal>
2023-05-18 14:03:43 +0000

    [work]商品新規登録

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,26 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
+    @genres = Genre.all
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
   end
 
   def show
   end
 
   def edit
+  end
+  
+  def item_params
+  params.require(:item).permit(:item_image, :name, :genr_id, :introduction, :price, :status)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ApplicationRecord
   belongs_to :genre
   belongs_to :cart_item
   has_many :order_details, dependent: :destroy
+  has_one_attached :item_image
 end
