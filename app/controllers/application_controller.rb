a143affae0a95f065b8c877a81767e43108eedba
EC2 Default User <ec2-user@ip-172-31-27-82.ap-northeast-1.compute.internal>
2023-06-06 14:06:05 +0000

    [finish]テスト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_admin!, if: :admin_url
 
-  def after_sign_up_path_for(resource)
-    public_customers_path
-  end
-  
-  def after_sign_in_path_for(resource)
-    root_path
-  end
-
   def admin_url
     request.fullpath.include?("/admin")
   end
